31b27a13c1180145593b2884ddb3f94d26d14af4
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-06-22 12:35:37 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -2,7 +2,7 @@ import groovy.json.JsonSlurper
 
 def aws_login(accessKeyId, secretAccessKey, region) {
     withCredentials([file(credentialsId: 'aws-credentials-id', variable: 'AWS_CREDENTIALS_FILE')]) {
-        sh 'cat $AWS_CREDENTIALS_FILE'
+        
         // Read and parse the JSON credentials file
         def jsonFile = readFile(env.AWS_CREDENTIALS_FILE)
         def credentials = new JsonSlurper().parseText(jsonFile)
