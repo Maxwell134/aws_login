a5eaa3407bd5b4d9a90a576158b3918505b7a5e1
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-06-22 12:19:58 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -1,23 +1,17 @@
 import groovy.json.JsonSlurper
 
-def aws_login(accessKeyId, secretAccessKey, region){
+def aws_login(accessKeyId, secretAccessKey, region) {
+    withCredentials([file(credentialsId: 'aws-credentials-id', variable: 'AWS_CREDENTIALS_FILE')]) {
+        // Read and parse the JSON credentials file
+        def jsonFile = readFile(env.AWS_CREDENTIALS_FILE)
+        def credentials = new JsonSlurper().parseText(jsonFile)
 
-withCredentials([file(credentialslId:'aws-credentials-id', variable: 'AWS_CREDENTIALS_FILE' )]){
+        // Use passed parameters or fallback to JSON credentials if not provided
+        accessKeyId = accessKeyId ?: credentials.accessKeyId
+        secretAccessKey = secretAccessKey ?: credentials.secretAccessKey
+        region = region ?: credentials.region
 
-// Read and parse the JSON credentials file
-
-def jsonFile = readFile(env.AWS_CREDENTIALS_FILE)
-def credentials = new JsonSlurper().parserText(jsonFile)
-
-def accessKeyId = credentials.accessKeyId
-def secretAccessKey = credentials.secretAccessKey
-def region = credentials.region
-
-return [accessKeyId, secretAccessKey, region]
-
-
+        return [accessKeyId, secretAccessKey, region]
+    }
 }
-
-
-}
-
+return this
